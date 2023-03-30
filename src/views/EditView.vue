b9b5a1fa329cea451b01dbc9bba9dc3147dda62d
Matt Miller <matt@mattmiller.dev>
2023-03-29 22:18:13 -0500

    fix: fix icon url label

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -28,7 +28,7 @@
 
           <FormKit id="repeater" name="links" type="repeater" label="Badges" :value="data.links" min="0" max="10">
             <FormKit type="select" label="Icon" name="icon_id" :options="linkIconOptions" />
-            <FormKit type="url" label="Url" name="url" validation="required|url" placeholder="Add site address" />
+            <FormKit type="url" label="URL" name="url" validation="required|url" placeholder="Add site address" />
           </FormKit>
 
           <FormKit id="repeater" name="buttons" type="repeater" label="Buttons" :value="data.buttons" min="0" max="10">
