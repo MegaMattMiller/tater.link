aa76d8ffa7aedcc8a2d20fd0c0be6168e7109e0c
Matt Miller <matt@mattmiller.dev>
2023-03-29 23:28:13 -0500

    feat: set up share in nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,15 @@
 <template>
   <nav class="nav-bar">
     <h1 class="header" @click="navigate('/')">Tater Link</h1>
-    <FormKit type="button" @click="navigate('/edit')" outer-class="nav-button">Edit</FormKit>
+    <FormKit
+      type="button"
+      @click.prevent="startShare"
+      v-if="isLoggedIn && isSupported && data != undefined"
+      outer-class="nav-button"
+      wrapper-class="share-button"
+      >Share</FormKit
+    >
+    <FormKit type="button" @click="navigate('/edit')" v-if="isLoggedIn" outer-class="nav-button">Edit</FormKit>
     <FormKit type="button" @click="handleSignOut" v-if="isLoggedIn" outer-class="nav-button">Logout</FormKit>
     <FormKit type="button" @click="navigate('/register')" v-if="!isLoggedIn" outer-class="nav-button">Register</FormKit>
     <FormKit type="button" @click="navigate('/signin')" v-if="!isLoggedIn" outer-class="nav-button">Sign In</FormKit>
@@ -12,10 +20,26 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { getAuth, onAuthStateChanged, signOut, type User } from 'firebase/auth';
+import { useShare } from '@vueuse/core';
+import { storeToRefs } from 'pinia';
+import { Icon } from '@iconify/vue';
+import { linkStore } from '@/stores/linkStore';
 
+const store = linkStore();
 const router = useRouter();
-const isLoggedIn = ref(true);
+const isLoggedIn = ref(false);
 const loggedInUser = ref<User | null>(null);
+const { data } = storeToRefs(store);
+
+const { share, isSupported } = useShare();
+
+function startShare() {
+  share({
+    title: 'Tater Link Card',
+    text: 'Check out my Tater Link Card!',
+    url: `https://tater.link/${data.value?.name}`,
+  });
+}
 
 // runs after firebase is initialized
 onAuthStateChanged(getAuth(), function (user) {
@@ -37,6 +61,14 @@ function navigate(path: string = '/') {
   router.push(path);
 }
 </script>
+
+<style lang="scss">
+// .share-button button {
+// background-color: transparent !important;
+// border-color: transparent !important;
+// color: #000 !important;
+// }
+</style>
 
 <style lang="scss">
 @import '@/styles/mixins.scss';
